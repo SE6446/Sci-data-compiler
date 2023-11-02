d81b093f52aa67018ff336f6e834c1285e0ff80d
Archie Macdonald <SE6446@cyber-wizard.com>
2023-11-02 03:52:40 -0800

    Fixed data-compiler and added line_plot

--- vernier_to_plt.py
+++ vernier_to_plt.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.optimize import curve_fit
+import scipy.optimize as opt;
 import pandas
 from math import isnan
 from statistics import mean
@@ -14,14 +14,19 @@ class CsvToPlt():
         self.columns = dataframes.columns
         data = dataframes.values
         data2 = []
+        
         for i in range(0,len(data)):
+            temp_dict = {}
             row = data[i]
-            temp_dict = {dataframes.columns[0]:row[0],dataframes.columns[1]:row[1],dataframes.columns[2]:row[2]}
+            for j in range(0,len(row)):
+                temp_dict[self.columns[j]] = row[j]
             data2.append(temp_dict)
         return data2
     
 
     def list_to_scatter(self,title:str,x_label,y_label,column_for_y:int = 1,lobf:bool = False,cobf:bool = False,save:bool = False,save_name:str|None = None,show:bool = True):
+        def func(x, a, b, c):
+            return a * np.exp(-b * x) + c
         x_axis = []
         y_axis = []
         for i in self.csv_list:
@@ -38,7 +43,15 @@ class CsvToPlt():
             plt.plot(x,a*x+b)
         elif cobf:
             #! Not implemented
-            raise NotImplementedError()
+            #raise NotImplementedError()
+            # Plot the actual data  
+            plt.plot(x, y, ".", label="Data")
+
+            # The actual curve fitting happens here
+            optimizedParameters, pcov = opt.curve_fit(func, x, y)
+
+            # Use the optimized parameters to plot the best fit
+            plt.plot(x, func(x, *optimizedParameters), label="fit")
         plt.title(title)
         plt.xlabel(x_label)
         plt.ylabel(y_label)
@@ -55,8 +68,9 @@ class CsvToPlt():
         return mean(column), max(column) - min(column)
     def compile_all_data(self,output_file):
         file = open(output_file+".🧪.json","w")
+        temp_list = []
         for i in range(1,len(self.columns)):
-            save_name = f"column_{i}_over_column_0"
+            save_name = f"column{i}_over_column0"
             self.list_to_scatter(f"{self.columns[i]} over {self.columns[0]}",
                                  self.columns[0],
                                  self.columns[i],
@@ -67,11 +81,45 @@ class CsvToPlt():
                                  save_name= save_name
                                  )
             plt.clf()
-        json.dump({"figure_location":save_name+".png","mean_and_range":self.find_mean_and_range(i),"csv-data":self.csv_list},file)
-    #TODO Make a subplot version of scatter.
+            self.plot_line(
+                f"{self.columns[i]} over {self.columns[0]}",
+                self.columns[0],
+                self.columns[i],
+                i,
+                True,
+                save_name + "_line",
+                False
+            )
+            plt.clf()
+            temp_list.append({"figure_location":save_name+".png","mean_and_range":self.find_mean_and_range(i)})
+        json.dump([temp_list,self.csv_list],file)
+    def plot_line(self,title:str,x_label,y_label,column_for_y:int = 1,save:bool = False,save_name:str|None = None,show:bool=True):
+        x_axis = []
+        y_axis = []
+        for i in self.csv_list:
+            if not isnan(i[self.columns[column_for_y]]):
+                y_axis.append(float(i[self.columns[column_for_y]]))
+                x_axis.append(float(i[self.columns[0]]))
+        #* convert to numpy
+        x = np.array(x_axis)
+        y = np.array(y_axis)
+        plt.plot(x,y)
+        plt.title(title)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        if save:
+            plt.savefig(save_name+".png")
+        if show:
+            #* show figure
+            plt.show()
+    #TODO Make normal line graph version.
+
 
 
 
 if __name__ == "__main__":
-    freefall = CsvToPlt("MT\csv-export-free-fall - Copy.csv")
-    freefall.compile_all_data("Freefall_data")
+    freefall = CsvToPlt("MT\Bouncing_ball - Copy.csv")
+    #freefall.list_to_scatter("Positon over time graph of a bouncing ball","time (s)","Position (m)",1,save=True,save_name="PT-bouncing-ball-copy",cobf=True)
+    #freefall.list_to_scatter("Velocity over time graph for a bouncing ball","time (s)","Velocity (ms^-1)",2)
+    #freefall.list_to_scatter("Accleration over time graph for a bouncing ball","time (s)","acceleration (ms^-2)",3)
+    freefall.compile_all_data("Bouncing_ball")
