f93b9c3563ca99afac81e5013a9f9932f50a02c9
Archie Macdonald <SE6446@cyber-wizard.com>
2023-11-08 03:53:18 -0800

    booboo inbound

--- vernier_to_plt.py
+++ vernier_to_plt.py
@@ -38,29 +38,29 @@ class CsvToPlt():
         return new_data
     
 
-    def list_to_scatter(self,title:str,x_label,y_label,column_for_y:int = 1,lobf:bool = False,cobf:bool = False,save:bool = False,save_name:str|None = None,show:bool = True,dataset:int = 0):
+    def list_to_scatter(self,title:str,x_label,y_label,column_for_y:int = 1,lobf:bool = False,cobf:bool = False,save:bool = False,save_name:str|None = None,show:bool = True,dataset:int = 0,colour:str = "r"):
         x,y = self._dataset_to_coords(dataset,column_for_y)
-        plt.scatter(x,y)
+        plt.scatter(x,y,color=colour)
         #*line of best fit
         try:
             if lobf:
                 a,b = np.polyfit(x,y,1)
                 self.gradient = a
                 self.y_intercept = b
-                plt.plot(x,a*x+b)
+                plt.plot(x,a*x+b,color=colour)
             #*curve of best fit
             elif cobf:
-                def func(x, a, b, c):
-                    print(x)
-                    return a * np.exp(-b * x) + c
-                #raise NotImplementedError()
-                # Plot the actual data  
-                plt.plot(x, y, ".",)
+                print("Warning! curve fitting can cause issues and some graphs may not appear")
+                #fit fourth-degree polynomial
+                model4 = np.poly1d(np.polyfit(x, y, 4))
 
-                optimizedParameters, pcov = opt.curve_fit(func, x, y)
+#define scatterplot
+                polyline = np.linspace(1, 15, 50)
 
-                # Use the optimized parameters to plot the best fit
-                plt.plot(x, func(x, *optimizedParameters))
+#add fitted polynomial curve to scatterplot
+                plt.plot(polyline, model4(polyline), '--', color='red')
+                plt.show()
+                
         except:
             print("graph failed, skipping...")
             plt.clf()
@@ -89,7 +89,7 @@ class CsvToPlt():
             return None
     
 
-    def compile_all_data(self,output_file,subject:str = "null",dataset:int=0):
+    def compile_all_data(self,output_file,subject:str = "null",dataset:int=0,lobf:bool=True,colour:str='b'):
         try:
             mkdir(f"{getcwd()}\\{output_file}")
         except:
@@ -103,11 +103,13 @@ class CsvToPlt():
                                  self.columns[0],
                                  self.columns[i],
                                  i,
-                                 lobf= True,
+                                 lobf= lobf,
+                                 cobf= not lobf,
                                  show= False,
                                  save= True,
                                  save_name= save_name,
-                                 dataset=dataset
+                                 dataset=dataset,
+                                 colour=colour
                                  )
             plt.clf()
             self.plot_line(
@@ -118,16 +120,17 @@ class CsvToPlt():
                 True,
                 save_name + "_line",
                 False,
-                dataset
+                dataset,
+                colour
             )
             plt.clf()
             temp_list.append({"figure_location":getcwd()+save_name+".png","mean_and_range":self.find_mean_and_range(i),"gradient":self.gradient,"y-intercept":self.y_intercept})
         json.dump([temp_list,self.csv_list],file)
 
 
-    def plot_line(self,title:str,x_label,y_label,column_for_y:int=1,save:bool = False,save_name:str|None = None,show:bool=True,dataset:int=0):
+    def plot_line(self,title:str,x_label,y_label,column_for_y:int=1,save:bool = False,save_name:str|None = None,show:bool=True,dataset:int=0,colour:str = 'b'):
         x,y = self._dataset_to_coords(dataset,column_for_y=column_for_y)
-        plt.plot(x,y)
+        plt.plot(x,y,color=colour)
         plt.title(title)
         plt.xlabel(x_label)
         plt.ylabel(y_label)
@@ -156,5 +159,5 @@ class CsvToPlt():
 
 if __name__ == "__main__":
     data = CsvToPlt(input("Input CSV file: "),3,False)
-    #freefall.list_to_scatter("test","time","?",dataset=3)
-    data.compile_all_data(input("Input name of output file: "))
+    data.list_to_scatter("test","time","?",dataset=1,cobf=True)
+    #data.compile_all_data(input("Input name of output file: "),lobf=False,colour='r')
